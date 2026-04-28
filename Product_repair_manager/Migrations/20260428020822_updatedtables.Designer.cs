4e5d59146d92ffff34d6e01231b9210e61dd3dba
Daniel Antiporda <ac146989@avcol.school.nz>
2026-04-28 14:17:21 +1200

    updated mirgration, and data

--- Product_repair_manager/Migrations/20260428020822_updatedtables.Designer.cs
+++ Product_repair_manager/Migrations/20260428020822_updatedtables.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Product_repair_manager.Migrations
 {
     [DbContext(typeof(Product_repair_managerContext))]
-    partial class Product_repair_managerContextModelSnapshot : ModelSnapshot
+    [Migration("20260428020822_updatedtables")]
+    partial class updatedtables
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Product_repair_manager/Migrations/20260428020822_updatedtables.cs
@@ -0,0 +1,90 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Product_repair_manager.Migrations
+{
+    /// <inheritdoc />
+    public partial class updatedtables : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "damage_report",
+                table: "Item_damages",
+                newName: "severity");
+
+            migrationBuilder.RenameColumn(
+                name: "Item_date",
+                table: "Item_damages",
+                newName: "date");
+
+            migrationBuilder.RenameColumn(
+                name: "ItemId",
+                table: "Item_damages",
+                newName: "ItemsId");
+
+            migrationBuilder.RenameColumn(
+                name: "item_damages_id",
+                table: "damages_report",
+                newName: "Item_damagesId");
+
+            migrationBuilder.RenameColumn(
+                name: "ClassId",
+                table: "damages_report",
+                newName: "ClassesId");
+
+            migrationBuilder.AddColumn<int>(
+                name: "StaffId",
+                table: "User",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<string>(
+                name: "damage_type",
+                table: "Item_damages",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "StaffId",
+                table: "User");
+
+            migrationBuilder.DropColumn(
+                name: "damage_type",
+                table: "Item_damages");
+
+            migrationBuilder.RenameColumn(
+                name: "severity",
+                table: "Item_damages",
+                newName: "damage_report");
+
+            migrationBuilder.RenameColumn(
+                name: "date",
+                table: "Item_damages",
+                newName: "Item_date");
+
+            migrationBuilder.RenameColumn(
+                name: "ItemsId",
+                table: "Item_damages",
+                newName: "ItemId");
+
+            migrationBuilder.RenameColumn(
+                name: "Item_damagesId",
+                table: "damages_report",
+                newName: "item_damages_id");
+
+            migrationBuilder.RenameColumn(
+                name: "ClassesId",
+                table: "damages_report",
+                newName: "ClassId");
+        }
+    }
+}
