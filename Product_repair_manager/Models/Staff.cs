2ffa9ce12df7ad61ae366ae658ae2e3a925a1e2d
Daniel Antiporda <ac146989@avcol.school.nz>
2026-05-21 11:05:02 +1200

    daily

--- Product_repair_manager/Models/Staff.cs
+++ Product_repair_manager/Models/Staff.cs
@@ -5,41 +5,59 @@ namespace Product_repair_manager.Models
     public class Staff
     {
         public int StaffId { get; set; }
+
+        [DataType(DataType.Text)]
+        [StringLength(100, ErrorMessage = "no such as the you wrote dosent exist please try again")]
+        [MaxLength(100), MinLength(1)]
         public string FirstName { get; set; }
+
+        [DataType(DataType.Text)]
+        [StringLength(100, ErrorMessage = "no such as the you wrote dosent exist please try again")]
+        [MaxLength(100), MinLength(1)]
         public string LastName { get; set; }
+
+        [DataType(DataType.EmailAddress)]
+        [StringLength(100, ErrorMessage = "no such as the you wrote dosent exist please try again")]
+        [RegularExpression(@"^[^@\s]+@[^@\s]+\.[^@\s]+$", ErrorMessage = "Invalid email")]
+        [MaxLength(100), MinLength(1)]
         public string Email { get; set; }
+
+        [DataType(DataType.Text)]
+        [StringLength(100, ErrorMessage = "no such as the you wrote dosent exist please try again")]
+        [MaxLength(100), MinLength(1)]
         public string role { get; set; }
 
     }
 
-    //public class person
-    //{
-    //    [ScaffoldColumn(false)]
-    //    User UserId { get; set; }
-    //    [DataType(DataType.Text)]
-    //    [StringLength(1, ErrorMessage = "try again")]
-    //    [Display(Name = "enter a report")]
-
-
-    //    public string FirstName { get; set; }
-    //    [DataType(DataType.Text)]
-    //    [StringLength(100, ErrorMessage = "no such as the you wrote dosent exist please try again")]
-    //    [MaxLength(100), MinLength(1)]
-
-    //    public string LastName { get; set; }
-    //    [DataType(DataType.Text)]
-    //    [StringLength(100, ErrorMessage = "no such as the you wrote dosent exist please try again")]
-    //    [MaxLength(100), MinLength(1)]
-
-    //    public string Email { get; set; }
-    //    [DataType(DataType.EmailAddress)]
-    //    [StringLength(100, ErrorMessage = "no such as the you wrote dosent exist please try again")]
-    //    [RegularExpression(@"^[^@\s]+@[^@\s]+\.[^@\s]+$", ErrorMessage = "Invalid email")]
-    //    [MaxLength(100), MinLength(1)]
-
-    //    public string role { get; set; }
-    //    [DataType(DataType.Text)]
-    //    [StringLength(100, ErrorMessage = "no such as the you wrote dosent exist please try again")]
-    //    [MaxLength(100), MinLength(1)]
-    //}
+   
 }
+//public class person
+//{
+//    [ScaffoldColumn(false)]
+//    User UserId { get; set; }
+//    [DataType(DataType.Text)]
+//    [StringLength(1, ErrorMessage = "try again")]
+//    [Display(Name = "enter a report")]
+
+
+//    public string FirstName { get; set; }
+//    [DataType(DataType.Text)]
+//    [StringLength(100, ErrorMessage = "no such as the you wrote dosent exist please try again")]
+//    [MaxLength(100), MinLength(1)]
+
+//    public string LastName { get; set; }
+//    [DataType(DataType.Text)]
+//    [StringLength(100, ErrorMessage = "no such as the you wrote dosent exist please try again")]
+//    [MaxLength(100), MinLength(1)]
+
+//    public string Email { get; set; }
+//    [DataType(DataType.EmailAddress)]
+//    [StringLength(100, ErrorMessage = "no such as the you wrote dosent exist please try again")]
+//    [RegularExpression(@"^[^@\s]+@[^@\s]+\.[^@\s]+$", ErrorMessage = "Invalid email")]
+//    [MaxLength(100), MinLength(1)]
+
+//    public string role { get; set; }
+//    [DataType(DataType.Text)]
+//    [StringLength(100, ErrorMessage = "no such as the you wrote dosent exist please try again")]
+//    [MaxLength(100), MinLength(1)]
+//}
--- Product_repair_manager/Models/User.cs
+++ Product_repair_manager/Models/User.cs
@@ -6,42 +6,58 @@ namespace Product_repair_manager.Models
     {
         public int UserId { get; set; }
         public int StaffId { get; set; }
+
+        [DataType(DataType.Text)]
+        [StringLength(100, ErrorMessage = "no such as the you wrote dosent exist please try again")]
+        [MaxLength(100), MinLength(1)]
         public string FirstName { get; set; }
+
+        [DataType(DataType.Text)]
+        [StringLength(100, ErrorMessage = "no such as the you wrote dosent exist please try again")]
+        [MaxLength(100), MinLength(1)]
         public string LastName { get; set; }
+
+        [DataType(DataType.EmailAddress)]
+        [StringLength(100, ErrorMessage = "no such as the you wrote dosent exist please try again")]
+        [RegularExpression(@"^[^@\s]+@[^@\s]+\.[^@\s]+$", ErrorMessage = "Invalid email")]
+        [MaxLength(100), MinLength(1)]
         public string Email { get; set; }
+
+        [DataType(DataType.Text)]
+        [StringLength(100, ErrorMessage = "no such as the you wrote dosent exist please try again")]
+        [MaxLength(100), MinLength(1)]
         public string role { get; set; }
 
     }
-
-    //public class person
-    //{
-    //    [ScaffoldColumn(false)]
-    //    User UserId { get; set; }
-    //    [DataType(DataType.Text)]
-    //    [StringLength(1, ErrorMessage = "try again")]
-    //    [Display(Name = "enter a report")]
-
-
-    //    public string FirstName { get; set; }
-    //    [DataType(DataType.Text)]
-    //    [StringLength(100, ErrorMessage = "no such as the you wrote dosent exist please try again")]
-    //    [MaxLength(100), MinLength(1)]
-
-    //    public string LastName { get; set; }
-    //    [DataType(DataType.Text)]
-    //    [StringLength(100, ErrorMessage = "no such as the you wrote dosent exist please try again")]
-    //    [MaxLength(100), MinLength(1)]
-
-    //    public string Email { get; set; }
-    //    [DataType(DataType.EmailAddress)]
-    //    [StringLength(100, ErrorMessage = "no such as the you wrote dosent exist please try again")]
-    //    [RegularExpression(@"^[^@\s]+@[^@\s]+\.[^@\s]+$", ErrorMessage = "Invalid email")]
-    //    [MaxLength(100), MinLength(1)]
-
-    //    public string role { get; set; }
-    //    [DataType(DataType.Text)]
-    //    [StringLength(100, ErrorMessage = "no such as the you wrote dosent exist please try again")]
-    //    [MaxLength(100), MinLength(1)]
-    
-    //}
 }
+//public class person
+//{
+//    [ScaffoldColumn(false)]
+//    User UserId { get; set; }
+//    [DataType(DataType.Text)]
+//    [StringLength(1, ErrorMessage = "try again")]
+//    [Display(Name = "enter a report")]
+
+
+//    public string FirstName { get; set; }
+//    [DataType(DataType.Text)]
+//    [StringLength(100, ErrorMessage = "no such as the you wrote dosent exist please try again")]
+//    [MaxLength(100), MinLength(1)]
+
+//    public string LastName { get; set; }
+//    [DataType(DataType.Text)]
+//    [StringLength(100, ErrorMessage = "no such as the you wrote dosent exist please try again")]
+//    [MaxLength(100), MinLength(1)]
+
+//    public string Email { get; set; }
+//    [DataType(DataType.EmailAddress)]
+//    [StringLength(100, ErrorMessage = "no such as the you wrote dosent exist please try again")]
+//    [RegularExpression(@"^[^@\s]+@[^@\s]+\.[^@\s]+$", ErrorMessage = "Invalid email")]
+//    [MaxLength(100), MinLength(1)]
+
+//    public string role { get; set; }
+//    [DataType(DataType.Text)]
+//    [StringLength(100, ErrorMessage = "no such as the you wrote dosent exist please try again")]
+//    [MaxLength(100), MinLength(1)]
+
+//}
