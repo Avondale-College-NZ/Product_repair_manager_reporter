40ac51ff6fe643d29e3731f661d3bbcc904920a1
Daniel Antiporda <ac146989@avcol.school.nz>
2026-06-04 11:10:52 +1200

    progress

---
+++ Product_repair_manager/Data/SQL/items locations in class.sql
@@ -0,0 +1,31 @@
+
+select
+Catagory_Name,
+CatagoryId
+from
+Catagory
+
+select
+items_Name,
+ItemsId,
+CatagoryId
+from
+Items
+
+select
+classroom,
+blocks
+from
+Classes;
+
+select
+A.CatagoryId,
+A.Catagory_name,
+B.items_Name,
+B.CatagoryId
+classroom,
+C.blocks
+from
+Classes C
+inner join Catagory A on C.Classroom = A.CatagoryId
+inner join Items B on A.CatagoryId = B.CatagoryId
